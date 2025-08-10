5c6c70623c5d1534b075f9edb262d1b92f2fa537
Himal <himalm100@gmail.com>
2025-08-10 09:11:49 -0700

    Added deployment condition

--- birthday.py
+++ birthday.py
@@ -8,7 +8,7 @@ import atexit
 import re
 from flask import Flask
 import threading
-
+import time
 
 # Mini Flask Server
 app = Flask(__name__)
@@ -186,7 +186,9 @@ def cleanup():
 
 
 if __name__ == "__main__":
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.daemon = True
-    flask_thread.start()
+    if "PORT" in os.environ:  # Running on Render
+        threading.Thread(target=run_flask, daemon=True).start()
+
+    time.sleep(2)
+
     bot.run(TOKEN)
